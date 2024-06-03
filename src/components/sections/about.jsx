add109a6431fba11c4a9f7544913e523c062ad4e
indextrown <lghan00020@nate.com>
2024-06-03 12:32:00 +0900

    Initial commit

---
+++ src/components/sections/about.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { RiArrowRightUpLine, RiDownloadLine, RiFacebookCircleFill, RiTwitterXLine, RiLinkedinFill, RiGithubLine } from '@remixicon/react'
+import profile_img from "../../assets/images/about/profile.jpg"
+import SlideUp from '../animations/slideUp'
+const About = () => {
+    return (
+        <section id="about" className="about-area">
+            <div className="container">
+                <div className="row align-items-center">
+                    {/*  START ABOUT IMAGE DESIGN AREA  */}
+                    <div className="col-lg-5">
+                        <SlideUp>
+                            <div className="about-image-part">
+                                <img src={profile_img} alt="About Me" />
+                                <h2>김동현</h2>
+                                <div className="about-btn btn-one text-center">
+                                    <h6>Available for Work</h6>
+                                    <div className="circle pulse color-pulse"></div>
+                                </div>
+                                <div className="about-social text-center">
+                                    <ul>
+                                        <li><a href=""><i><RiFacebookCircleFill size={20} /></i></a></li>
+                                        <li><a href=""><i><RiTwitterXLine size={20} /></i></a></li>
+                                        <li><a href=""><i><RiLinkedinFill size={20} /></i></a></li>
+                                        <li><a href=""><i><RiGithubLine size={20} /></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </SlideUp>
+                    </div>
+                    {/*  END ABOUT IMAGE DESIGN AREA  */}
+                    {/*  START ABOUT TEXT DESIGN AREA  */}
+                    <div className="col-lg-7">
+                        <div className="about-content-part">
+                            <SlideUp>
+                                <h2>
+                                    {/* 선한 영향력을 나누고 싶은 <br></br>
+                                    iOS 개발자 <span>김동현</span> 입니다.  */}
+                                    <h1>I'm <span>iOS</span> developer</h1>
+                                </h2>
+                                <div className="hero-btns">
+                                    <a href="#" className="theme-btn">Download CV <i><RiDownloadLine size={16} /></i></a>
+                                </div>
+                            </SlideUp>
+                            <SlideUp>
+                                <ul className="list-style-one two-column">
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Logo Design</li>
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Social Marketing</li>
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Branding Identity</li>
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Digital Marketing</li>
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Web Design</li>
+                                    <li><i><RiArrowRightUpLine size={18} /></i> Product Design</li>
+                                </ul>
+                            </SlideUp>
+                        </div>
+                    </div>
+                    {/*  END ABOUT TEXT DESIGN AREA  */}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
